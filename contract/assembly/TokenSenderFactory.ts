f6ba78463e4eda38247b56898fd098a646b12a99
xleonhunter <xleonhunter@gmail.com>
2021-12-23 14:32:51 -0500

    resolved typo in TokenSenderFactory

--- contract/assembly/TokenSenderFactory.ts
+++ contract/assembly/TokenSenderFactory.ts
@@ -1,16 +1,16 @@
-import { TokenSender } from './TokenSender';
-import { TokenSenderLoggerImpl } from './TokenSenderLoggerImpl';
+import { TokenSenderImpl } from './TokenSenderImpl';
+import { TokenSenderLogger } from './TokenSenderLogger';
 import { TokenSenderInterface } from './TokenSenderInterface';
 import { PersistentMap } from 'near-sdk-as'
 
 export class TokenSenderFactory {
 
     static getNew(senderMap:PersistentMap<string, string[]>, receiverMap:PersistentMap<string, i32[]>): TokenSenderInterface {
-        return new TokenSender(senderMap, receiverMap);
+        return new TokenSenderImpl(senderMap, receiverMap);
     }
 
     static getLogged(tokenSender:TokenSenderInterface):TokenSenderInterface {
-        return new TokenSenderLoggerImpl(tokenSender);
+        return new TokenSenderLogger(tokenSender);
     }
     
     static getDefault(): TokenSenderInterface {
